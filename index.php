e1b8e4d3b6c6c648d831e14d62e871a51c27ae39
OzzyOs <Joshahursepuny@gmail.com>
2022-01-11 13:12:33 +0100

    <button> is nu <input type="button" value="Afspraken"

--- index.php
+++ index.php
@@ -1,20 +1,59 @@
-<?php
-?>
 
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Music Collection</title>
+    <title>Reserveringssysteem</title>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
+
 <body>
-<h1>Welkom bij [INSERT NAME]</h1>
-<nav>
-    <button onclick="location.href='registratie.php'">Registreren</button>
+<h1>WELKOM</h1>
+
+<nav class="sidenav">
+    <button onclick="location.href='index.php'">Home</button>
+    <button onclick="location.href='registratie.php'">Afspraak</button>
     <button onclick="location.href='login.php'">Login</button>
     <button onclick="location.href='contact.php'">Contact</button>
+    <button onclick="location.href='afspraken.php'">Afspraken</button>
 </nav>
 
+<div class="wrapper">
+    <div class="main-wrapper1">
+            <div class="main-wrapper-div1">
+            </div>
+            <div class="main-wrapper-div2">
+                <img src="includes/background.png" alt="" width="700" height="300">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a mattis libero. Nam fringilla, sem nec lacinia iaculis, risus diam posuere lectus, eget porta erat metus a magna. Phasellus volutpat felis nulla, eu tempus diam congue eget. Aliquam tellus elit, convallis quis cursus sit amet, efficitur eget tellus. Aliquam tristique tincidunt ornare. Duis sodales leo nibh, sit amet scelerisque justo dignissim pharetra. Pellentesque nec commodo ante, non porta enim.
+                </p><br>
+            </div>
+    </div>
+
+    <div class="second-wrapper2">
+            <div class="second-wrapper-div1">
+                <div class="second-wrapper-div2">
+                    <p>
+                    <img src="includes/background.png" height="100px" width="220px">
+                    Traject
+                <input class="button-one" type="button" value="Afspraken" onclick="location.href='contact.php'">
+                    </p>
+            </div>
+            <div class="second-wrapper-div3">
+                <p>
+                    <img src="includes/background.png" height="100px" width="220px">
+                    Traject
+                    <input class="button-one" type="button" value="Afspraken" onclick="location.href='contact.php'">
+                </p>
+            </div>
+                <div class="second-wrapper-div4">
+                <p>
+                    <img src="includes/background.png" height="100px" width="220px">
+                    Traject
+                    <input class="button-one" type="button" value="Afspraken" onclick="location.href='contact.php'">
+                </p>
+                </div>
+            </div>
+    </div>
+</div>
 </body>
 </html>
