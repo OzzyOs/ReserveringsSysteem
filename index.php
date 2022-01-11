ddab6983d8f2594a931c0e449c074f838fed93d0
OzzyOs <Joshahursepuny@gmail.com>
2022-01-11 15:50:12 +0100

    <button> in nav nu <input type="button"> hoofdtekst binnen "main-wrapper-div2" veranderd naar class="main-text".

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 <h1>WELKOM</h1>
 
 <nav class="sidenav">
-    <button onclick="location.href='index.php'">Home</button>
-    <button onclick="location.href='registratie.php'">Afspraak</button>
-    <button onclick="location.href='login.php'">Login</button>
-    <button onclick="location.href='contact.php'">Contact</button>
-    <button onclick="location.href='afspraken.php'">Afspraken</button>
+    <input type="button" value="Home" onclick="location.href='index.php'">
+    <input type="button" value="Afspraak" onclick="location.href='registratie.php'">
+    <input type="button" value="Login" onclick="location.href='login.php'">
+    <input type="button" value="Contact" onclick="location.href='contact.php'">
+    <input type="button" value="Afspraken" onclick="location.href='afspraken.php'">
 </nav>
 
 <div class="wrapper">
@@ -23,8 +23,8 @@
             <div class="main-wrapper-div1">
             </div>
             <div class="main-wrapper-div2">
-                <img src="includes/background.png" alt="" width="700" height="300">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a mattis libero. Nam fringilla, sem nec lacinia iaculis, risus diam posuere lectus, eget porta erat metus a magna. Phasellus volutpat felis nulla, eu tempus diam congue eget. Aliquam tellus elit, convallis quis cursus sit amet, efficitur eget tellus. Aliquam tristique tincidunt ornare. Duis sodales leo nibh, sit amet scelerisque justo dignissim pharetra. Pellentesque nec commodo ante, non porta enim.
+                <img class="home-img" src="includes/background.png" alt="" width="500" height="300">
+                <p class="main-text">DEBBY JORIS COACHING
                 </p><br>
             </div>
     </div>
@@ -34,21 +34,21 @@
                 <div class="second-wrapper-div2">
                     <p>
                     <img src="includes/background.png" height="100px" width="220px">
-                    Traject
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a mattis libero. Nam fringilla, sem nec lacinia iaculis, risus diam posuere lectus, eget porta erat metus a magna.
                 <input class="button-one" type="button" value="Afspraken" onclick="location.href='contact.php'">
                     </p>
             </div>
             <div class="second-wrapper-div3">
                 <p>
                     <img src="includes/background.png" height="100px" width="220px">
-                    Traject
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a mattis libero. Nam fringilla, sem nec lacinia iaculis, risus diam posuere lectus, eget porta erat metus a magna.
                     <input class="button-one" type="button" value="Afspraken" onclick="location.href='contact.php'">
                 </p>
             </div>
                 <div class="second-wrapper-div4">
                 <p>
                     <img src="includes/background.png" height="100px" width="220px">
-                    Traject
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a mattis libero. Nam fringilla, sem nec lacinia iaculis, risus diam posuere lectus, eget porta erat metus a magna.
                     <input class="button-one" type="button" value="Afspraken" onclick="location.href='contact.php'">
                 </p>
                 </div>
